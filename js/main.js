d40652678f8259092956983ea99e3605cbc6fe09
Siuanny Barbosa <siuanny.rocha@ccc.ufcg.edu.br>
2019-01-23 22:06:06 -0300

    add dedicacao

--- js/main.js
+++ js/main.js
@@ -26,3 +26,35 @@ function initClock() {
 }
 
 // END CLOCK SCRIPT
+
+var pos = 0,
+dpos = 3.5714,
+cycle,
+heart = false,
+animating = false;
+
+$('heart').click(function(){},function(){
+  if(heart && !animating){
+    $('heart').css('background-position','0 0');
+    heart = false;
+  }else if(!animating){
+    animating = true;
+    animate();
+  }
+});
+
+function animate(){
+  cycle = setInterval(increment,30);
+}
+
+function increment(){
+  pos += dpos;
+  if(pos > 100){
+    clearInterval(cycle);
+    heart = true;
+    animating = false;
+    pos = 0;
+  }else{
+    $('heart').css('background-position',pos+'% 0');
+  }
+}
